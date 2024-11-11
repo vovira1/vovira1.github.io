d9f404e5445a429093da14f352cb03195e1f4a8c
vovira1 <gulonatalia2016@gmail.com>
2024-11-11 14:26:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+// let apiKey = 'sk-proj-XKB2ACw3ImhbSEervL96T3BlbkFJXu2zrbkZFM5kQ88hbki8';
+let apiKey = 'sk-proj-TQA2rzfkgMhrnTTznFxWT3BlbkFJ4IZqmLMKtXNHGlK6ZjJG';
+
+let sendButton = window.document.getElementById('sendButton');
+
+let inp = window.document.getElementById('textInput');
+
+let outp = window.document.getElementById('textOutput');
+
+let conversation = [];
+
+let speechRecognizer = new webkitSpeechRecognition();    // recunoaștere vocală
+
+let speechSynthesis = window.speechSynthesis;            // sinteză vocală
+
+const speech = () => {
+speechRecognizer.lang = 'ro-RO';
+// speechRecognizer.continuous = true;
+// speechRecognizer.interimResults = true;
+speechRecognizer.start();
+sendButton.innerText = 'Vorbiți...';
+}
+
+const talk = (text) => {
+let textToTalk = new SpeechSynthesisUtterance(text);
+textToTalk.onend = function(event) {
+ sendButton.innerText = 'Doriți să mai spuneți ceva? Apasați aici';
+};
+textToTalk.lang = 'ro-RO';
+textToTalk.rate = 0.5;
+textToTalk.pitch = 0.1;
+speechSynthesis.speak(textToTalk);
+}
+
+let request = axios.create({                             // integrare Chat GPT
+headers: {
+Authorization: `Bearer ${apiKey}`
+}
+})
+
+speechRecognizer.onresult = (event) => {                 // Procesor de Eveniment
+inp.value = event.results[0][0].transcript;
+requestFunc();
+}
+
+const requestFunc = () => {
+if (inp.value) {
+sendButton.innerText = 'O clipă...';
+let message = {
+"role": "user",
+"content": `${inp.value}`
+}
+conversation.push(message);
+let params = {
+"model": "gpt-3.5-turbo",
+"messages": conversation
+};
+request.post('https://api.openai.com/v1/chat/completions', params)
+.then(response => {
+outp.value = response.data.choices[0].message.content;
+let gptMessage = {
+"role": "assistant",
+"content": `${outp.value}`
+}
+conversation.push(gptMessage);
+talk(outp.value);
+})
+}
+}
